32e56b81f52b883429772cfae3f589558869871f
Luka Altunsahvili <laltunashvili@radford.edu>
2019-11-17 17:53:48 -0500

    added accounts setttings routing and page

--- donuts/urls.py
+++ donuts/urls.py
@@ -31,12 +31,7 @@ urlpatterns = [
     path('thankyou/', views.thankyou, name='thankyou'),
     path('accounts/change_password/', views.change_password, name='change_password'),
     path('orders/', OrderListView.as_view(), name='orders'),
-<<<<<<< HEAD
     path('accounts/settings/', views.change_settings, name='change_settings'),
-
-=======
-    path('categories/', CategoryListView.as_view(), name='orders'),
->>>>>>> 79b3f934505781d979b95f6122d759c05d6465fd
 ]
 
 urlpatterns += [
@@ -45,7 +40,6 @@ urlpatterns += [
 
 item_list = {
     'queryset' : InventoryItem.objects.all(),
-    # 'template_name' : 'ordering_page.html/'
 }
 order_list = {
     'queryset': Order.objects.all(),
