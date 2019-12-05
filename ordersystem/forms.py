cd7be1be670824cb697e34d91e058499233f36a2
Lauren Rose <larose@radford.edu>
2019-12-05 00:06:23 -0500

    janky fix

--- ordersystem/forms.py
+++ ordersystem/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserAccount
+from .models import UserAccount, Cart
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -8,28 +8,13 @@ class RegisterForm(UserCreationForm):
         model = UserAccount
         fields = ('username', 'first_name', 'last_name', 'email')
 
+
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
+        user_cart = Cart(customer=user.id)
+        user_cart.save()
 
         if commit:
             user.save()
 
         return user
-
-#
-# class UserForm(forms.ModelForm):
-#     class Meta:
-#         model = UserAccount
-#         widgets = {'password': forms.PasswordInput()}
-#         fields = ['username',
-#                   'first_name',
-#                   'last_name',
-#                   'email',
-#                   'password']
-#
-#     def save(self, commit=True):
-#         user = super(UserForm, self).save(commit=False)
-#         user.username = ['username']
-#         user.first_name = ['first_name']
-#         user.last_name = ['last_name']
-#         user.email = ['email']
