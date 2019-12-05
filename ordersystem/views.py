c9ffa8ee237db70fe02d2365ed5c1aa4c0962368
Bradleyjl65 <32908758+Bradleyjl65@users.noreply.github.com>
2019-12-05 01:06:02 -0500

    add pay button to view cart

--- ordersystem/views.py
+++ ordersystem/views.py
@@ -91,16 +91,12 @@ def thankyou(request):
 
 
 class payment_page(TemplateView):
-    template_name = 'payments.html'
+    template_name = 'view_cart.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['key'] = settings.STRIPE_PUBLISHABLE_KEY
         return context
-
-    def payment(self):
-        cart = ItemCartRelationship.objects.all()
-        return render(self, 'payments.html', {"cart": cart})
 
 
 def charge(request):
